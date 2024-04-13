360d6a52ae7cb2e6c357caf23d8d7fc0e51c139b
evyz <endermainYT2502@gmail.com>
2024-04-13 23:57:31 +0300

    header updates

---
+++ scripts/burger.js
@@ -0,0 +1,54 @@
+const BURGER_WIDTH = 540
+const burgerBtn = document.getElementById('burger_btn')
+
+
+function getNodeFromChild(elem, nodeName){
+    const node = document.createElement(nodeName)
+    node.innerHTML = elem.innerHTML
+    return node
+}
+
+function onResize(e){
+    const header = document.querySelector('header')
+
+
+    if(window.innerWidth <= BURGER_WIDTH && !header.classList.contains('burger')){
+        const [ _logo, list, contacts ] = Array.from(header.children)
+        const nav = document.createElement('nav')
+        const navPopup = document.createElement('div')
+        navPopup.classList.add('popup')
+        nav.appendChild(navPopup)
+        navPopup.appendChild(getNodeFromChild(list, 'ul'))
+        navPopup.appendChild(getNodeFromChild(contacts, 'div'))
+        header.appendChild(nav)
+        header.classList.add('burger')
+
+        burgerBtn.onclick = () => {
+            nav.classList.toggle('active')
+        }
+
+        nav.onclick = (e) => {
+            e.stopPropagation()
+            nav.classList.remove('active')
+        }
+
+        navPopup.onclick =(e) => {
+            e.stopPropagation()
+        }
+    }else{
+        const content = header.children.item(header.children.length - 1)
+        if(content.nodeName !== "NAV"){
+            return
+        }
+
+        header.removeChild(content)
+        header.classList.remove('burger')
+
+    }
+
+
+
+}
+
+window.addEventListener('resize', onResize)
+onResize()
