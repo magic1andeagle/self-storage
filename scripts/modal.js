74253d1965e3513e9fa077da5b8b2a3e29a8d634
magic1andeagle <maks.fomichev.94@mail.ru>
2024-04-15 15:16:45 +0400

    added post request for modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,11 +1,44 @@
-const btn = document.getElementById("toggleModal");
+const button = document.getElementById("toggleModal");
 const modal = document.getElementById("consultModal");
 const closeBtn = document.getElementById("cancelButton");
+const sendButton = document.getElementById("sendButton");
+const nameInputModal = document.getElementById("nameInputModal");
+const phoneInputModal = document.getElementById("phoneInputModal");
 
-btn.onclick = function (ev) {
+const url = "https://ssttoorree.ru/_receive_question_";
+
+button.onclick = function (ev) {
   modal.style.display = "block";
 };
 
 closeBtn.onclick = function (ev) {
   modal.style.display = "none";
+};
+
+sendButton.onclick = function (ev) {
+  const data = {
+    firstname: nameInputModal.value,
+    username: phoneInputModal.value,
+    from: "store_engineering",
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Error!");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log("Успех:", data);
+    })
+    .catch((err) => {
+      console.log("Произошла ошибка:", err);
+    });
 };
